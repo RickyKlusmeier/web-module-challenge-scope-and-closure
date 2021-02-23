b5dc488523bd1963609f5b15ba0b751d09ea7c33
Ricky Klusmeier <rickyklusmeier@Rickys-MacBook-Pro.local>
2021-02-22 21:15:04 -0700

    Task 4

--- index.js
+++ index.js
@@ -105,9 +105,15 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningFunc) {
+  return {
+    "Home": inningFunc(),
+    "Away": inningFunc(),
+  };
 }
+
+console.log(finalScore(inning)
+
 
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
@@ -151,7 +157,7 @@ Use the scoreboard function below to do the following:
 ]  
   */
 
-function scoreboard(/* CODE HERE */) {
+function scoreboard(inningFunc, ) {
   /* CODE HERE */
 }
 
